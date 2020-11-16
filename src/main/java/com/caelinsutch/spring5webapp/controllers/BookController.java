39afa134d1aad6ca18ae2ff181feda8f467d7428
Caelin Sutch <caelinsutch@gmail.com>
2020-11-15 20:36:09 -0800

    Added a BookController

---
+++ src/main/java/com/caelinsutch/spring5webapp/controllers/BookController.java
@@ -0,0 +1,24 @@
+package com.caelinsutch.spring5webapp.controllers;
+
+import com.caelinsutch.spring5webapp.repositories.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model) {
+
+        model.addAttribute("books", bookRepository.findAll());
+
+        return "books";
+    }
+}
